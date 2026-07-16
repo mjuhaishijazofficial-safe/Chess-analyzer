06e19b1dbe36ccfc61736d90d4432694dbfe6a37
Zuhair <mzuhair.official@gmail.com>
2026-07-16 11:02:47 +0000

    Jul 16, 2026, 4:02 PM

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -55,11 +55,18 @@ export default function OpengraphImage() {
             border: "2px solid #1c222b",
             alignItems: "center",
             justifyContent: "center",
-            fontSize: 46,
             marginBottom: 28,
           }}
         >
-          ♞
+          <svg width="46" height="46" viewBox="0 0 24 24" fill="none">
+            <path
+              d="M19 22H5v-2h14v2zM17.5 20l-.5-4.5c-1.5-1-2.5-2.7-2.5-4.5 0-.9.3-1.7.7-2.4-.5-.3-1-.6-1.2-1.1-.3-.6-.2-1.3.2-1.9.6-.9.4-1.9-.4-2.5-.5-.4-1.2-.4-1.7 0L10.5 5c-.3.2-.5.6-.5 1v1.3c-1.2.5-2 1.7-2 3 0 .5.1 1 .3 1.4-.8.7-1.3 1.8-1.3 2.9 0 1.8 1 3.5 2.5 4.5l-.5 4.5"
+              stroke="#4ade80"
+              strokeWidth="1.6"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
         </div>
 
         <div
